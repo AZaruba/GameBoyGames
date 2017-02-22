1ffee47fff15effaefc3480095c282cb7a24c212
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-02-22 00:59:38 -0600

    Tiles mapped correctly, display still broken

--- gbSource/backgroundTest.c
+++ gbSource/backgroundTest.c
@@ -1,16 +1,19 @@
 #include <gb/gb.h>
-#include "Sprites/floor.c"
-#include "Sprites/heart.c"
+#include "bkgTiles/levelOne.c"
+#include "bkgTiles/castle.c"
 
 /*
  * Specific test for getting backgrounds working
  */
 void main(void) {
 
+	wait_vbl_done();
 	DISPLAY_OFF;
-    wait_vbl_done();
-    set_bkg_data(0, 1, floor);
-    set_bkg_tiles(0, 0, 0, 0, floor);
+    set_bkg_data(0x00,0x13, castle);
+    VBK_REG = 1;
+    VBK_REG = 0;
+    set_bkg_tiles(0x00, 0x00, 0x12, 0x12, levelOne);
+
     SHOW_BKG;
 
     DISPLAY_ON;
