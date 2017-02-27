118066fb9c9723a17109e3ffef794a2a9c52c554
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-02-26 23:55:22 -0600

    controls now work in tandem and collision is partially working

--- gbSource/movementTest.c
+++ gbSource/movementTest.c
@@ -12,11 +12,15 @@
 typedef struct {
     UINT8 x;
     UINT8 y;
+    INT8 g;
     UINT8 state;
 } spriteData;
 
 UINT8 i; // global loop integer
 
+/*
+ * loads player sprite
+ */
 void loadSprites(spriteData * spritePtr) {
     set_sprite_data(0x00, 0x0C, sabre);
     set_sprite_tile(0x00, 0x00);
@@ -34,7 +38,10 @@ void loadSprites(spriteData * spritePtr) {
     move_sprite(1, spritePtr->x + 8, spritePtr->y);
 }
 
-updatePos(spriteData * ptr) {
+/*
+ * updates the position of the PC
+ */
+void updatePos(spriteData * ptr) {
 	move_sprite(4, ptr->x, ptr->y + 16);
     move_sprite(5, ptr->x + 8, ptr->y + 16);
     move_sprite(2, ptr->x, ptr->y + 8);
@@ -43,16 +50,48 @@ updatePos(spriteData * ptr) {
     move_sprite(1, ptr->x + 8, ptr->y);
 }
 
+/*
+ * checks collision between player character and various objects
+ */
+UINT8 collision(spriteData * ptr) {
+	UINT8 edges = 0x00;
+	if (ptr->y <= 112) {
+		ptr->y = 112;
+		edges += 0x01;
+	}
+	return edges;
+}
+
+void gravity(spriteData * ptr) {
+    if ((collision(ptr) & 0x01) && ptr->g <= 0) {
+    	ptr->g = 0;
+    	return;
+    }
+    else {
+    	if (ptr->g > -2) {
+    	    ptr->g--;
+    	}
+    	ptr->y = ptr->y - ptr->g;
+    }
+}
+
+/*
+ * gathers user input and updates the position of the PC
+ */
 void userInput(spriteData * ptr) {
-    if (joypad()==J_RIGHT && ptr->x < 153) {
+
+    if (joypad() & J_RIGHT && ptr->x < 153) {
         ptr->x++;
-        updatePos(ptr);
     }
 
-    if (joypad() == J_LEFT && ptr->x > 0) {
+    if (joypad() & J_LEFT && ptr->x > 7) {
     	ptr->x--;
-    	updatePos(ptr);
     }
+    if (joypad() & J_A  && (collision(ptr) & 0x01) ) {
+    	ptr->g = 5;
+    }
+    gravity(ptr);
+    updatePos(ptr);
     delay(16);
 }
 
@@ -60,8 +99,9 @@ void main(void) {
 	// allocate space for sprite position information
     spriteData * witch = malloc(sizeof(spriteData));
     witch->state = 0x00;
+    witch->g = 0;
     witch->x = 31;
-    witch->y = 96;
+    witch->y = 40;
 
     // pre-start prep
 	wait_vbl_done();
@@ -88,5 +128,6 @@ void main(void) {
 
     while(1) {
         userInput(witch);
+
     }
 }
